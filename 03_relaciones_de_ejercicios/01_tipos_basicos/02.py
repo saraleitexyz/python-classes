429ad503448c760288bd5dab1d0326d72161a55c
Sara Leite <saraleite.mail@gmail.com>
2025-10-16 22:31:58 +0200

    exercise 02 from relation 01

---
+++ 03_relaciones_de_ejercicios/01_tipos_basicos/02.py
@@ -0,0 +1,14 @@
+# Pedir el radio de un círculo y calcular su área. A=PI*r²
+
+import math
+
+print("CALCULADORA DEL ÁREA DE UN CÍRCULO:")
+print("-----------------------------------")
+
+radio = float(input("Introduzca el radio del circulo: "))
+
+area = math.pi * (radio ** 2)
+
+print(f"El área del círculo de radio {radio} es: {area:.2f}")
+
+
