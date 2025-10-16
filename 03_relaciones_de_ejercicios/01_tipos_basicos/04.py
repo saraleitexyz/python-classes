28c57695bd5b8b39f91bd5558b2ecc496218a71d
Sara Leite <saraleite.mail@gmail.com>
2025-10-16 22:42:17 +0200

    exercise 04 from relation 01

---
+++ 03_relaciones_de_ejercicios/01_tipos_basicos/04.py
@@ -0,0 +1,12 @@
+  # 4. Pedir dos números y decir si son iguales o no.
+
+print("COMPROBADOR DE NÚMEROS IGUALES:")
+print("----------------------------------------------")
+
+num1 = int(input("Introduzca el primer número: "))
+num2 = int(input("Introduzca el segundo número: "))
+
+if num1 == num2:
+    print(f"Los números {num1} y {num2} son iguales.")
+else:
+    print(f"Los números {num1} y {num2} no son iguales.")
