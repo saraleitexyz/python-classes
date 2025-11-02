c592430c4b694b60bc11064f20809a1801e40662
Sara Leite <saraleite.mail@gmail.com>
2025-11-02 19:39:09 +0100

    Added more exercises

---
+++ 02_relaciones_de_ejercicios/00_teoria/0022_pizzas.py
@@ -0,0 +1,6 @@
+pizza_kinds = ["pineapple", "bbq", "cheese and ham"]
+
+for pizza_kind in pizza_kinds:
+    print(f"I would like a {pizza_kind} pizza now!")
+
+print("I really love pizza!")
---
+++ 02_relaciones_de_ejercicios/00_teoria/0028_one_million.py
@@ -0,0 +1,7 @@
+one_million_numbers = list(range(1, 1_000_001))
+for number in one_million_numbers:
+    print(number)
+
+print(min(one_million_numbers))
+print(max(one_million_numbers))
+print(sum(one_million_numbers))
