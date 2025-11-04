0e7bf0299076cd3c75c214404a35777b0f1da12e
Sara Leite <saraleite.mail@gmail.com>
2025-11-04 21:48:11 +0100

    Correctedfrom rel1 exercises 1 to 9

--- 02_relaciones_de_ejercicios/01_tipos_basicos/01.py
+++ 02_relaciones_de_ejercicios/01_tipos_basicos/01.py
@@ -16,7 +16,7 @@ elif discriminante < 0:
     print("No existen soluciones reales.")
 elif discriminante == 0:
     x = -b / (2 * a)
-    print("Existe una única solución ral:", x)
+    print("Existe una única solución real:", x)
 else:
     x1 = (-b + math.sqrt(discriminante)) / (2 * a)
     x2 = (-b - math.sqrt(discriminante)) / (2 * a)
