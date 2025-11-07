0f1947a1096991a56f48ccde589ea64af74e092f
Sara Leite <saraleite.mail@gmail.com>
2025-11-07 19:07:59 +0100

    Reached page 195 of the python crash course book

---
+++ 02_relaciones_de_ejercicios/00_teoria_crash_course/0068_rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("How tall are you, in centimetres? ")
+height = int(height)
+
+if height >= 150:
+    print("\nYou are tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
